a4698b9773c6a9a1ae580d65e9d73bc41a3b7f82
duckiss <duongduc936@gmail.com>
2025-03-14 15:21:48 +0700

    new

---
+++ Ecommerce-beeshoes-frontend/src/AppConfig.js
@@ -0,0 +1,11 @@
+/** @format */
+
+export const AppConfig = {
+  apiUrl: "http://beeshoes-backend-service:8080",
+  routerBase: "",
+};
+
+export const AppConfigAddress = {
+  apiUrl: "",
+  routerBase: "",
+};
